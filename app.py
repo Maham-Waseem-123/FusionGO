34bd6c5ebcc7f2bee2b1e488e1bf2592c48d89a7
Maham Waseem <mahamwaseem316@gmail.com>
2025-07-04 20:56:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -18,7 +19,6 @@ def services():
 def contact():
     return render_template('contact.html')
 
-# Add these new routes
 @app.route('/careers')
 def careers():
     return render_template('careers.html')
@@ -26,14 +26,6 @@ def careers():
 @app.route('/investing')
 def investing():
     return render_template('investing.html')
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
